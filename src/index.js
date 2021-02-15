7ccfcd06a077948c442ff20a6c2d25afc992503f
RadhikaRJ <radhika7002@gmail.com>
2021-02-15 21:22:10 +0530

    calculate Winner displayed

--- src/index.js
+++ src/index.js
@@ -32,6 +32,10 @@ class Board extends React.Component {
 
   handleClick(i){
   const squares = this.state.squares.slice(); //.slice() creates a copy of the squares
+  if(calculateWinner(squares) || squares[i]){
+    return;
+  }
+  
   squares[i]= this.state.xIsNext ? 'X':'O';
   this.setState({
     squares: squares ,
@@ -50,7 +54,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext?'X':'O');
+    // const status = 'Next player: ' + (this.state.xIsNext?'X':'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner){
+      status="Winner: "+winner;
+    }else{
+      status="Next Player: " + (this.state.xIsNext?'X':'O');
+    }
 
     return (
       <div>
